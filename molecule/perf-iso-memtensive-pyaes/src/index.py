3110db743b144013ca1374ccfc237fa9deb20c11
Dong Du <ddnirvana1@gmail.com>
2021-04-27 21:30:48 +0800

    add three cases for perf-iso
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ molecule/perf-iso-memtensive-pyaes/src/index.py
@@ -0,0 +1,55 @@
+from time import time
+import random
+import string
+import pyaes
+
+
+def generate(length):
+    letters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def handler(event):
+    length_of_message = event['length_of_message']
+    num_of_iterations = event['num_of_iterations']
+
+    message = generate(length_of_message)
+
+    # 128-bit key (16 bytes)
+    KEY = b'\xa1\xf6%\x8c\x87}_\xcd\x89dHE8\xbf\xc9,'
+
+    start = time()
+    for loops in range(num_of_iterations):
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        ciphertext = aes.encrypt(message)
+        #print(ciphertext)
+
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        plaintext = aes.decrypt(ciphertext)
+        #print(plaintext)
+        aes = None
+
+    latency = time() - start
+    return latency
+
+def invokeHandler():
+    startTime = int(round(time() * 1000))
+    count = 1
+    for i in range(1, 1000000000000):
+        count+=1
+        #ret = handler({'length_of_message': 256, 'num_of_iterations':10})
+
+    retTime = int(round(time() * 1000))
+
+    output = {'results': ret,
+        'startTime': startTime,
+        'retTime' : retTime,
+        'invokeTime': startTime
+        }
+    #logf = open("log.txt", "w")
+    #logf.write(str(output))
+
+    #print(output)
+
+if __name__ == "__main__":
+    invokeHandler()
---
+++ molecule/perf-iso-source-matmul/src/index.py
@@ -0,0 +1,42 @@
+import numpy as np
+from time import time
+
+
+def matmul(n):
+    A = np.random.rand(n, n)
+    B = np.random.rand(n, n)
+
+    start = time()
+    C = np.matmul(A, B)
+    latency = time() - start
+    return latency
+
+
+def handler(event):
+    n = int(event['n'])
+    result = matmul(n)
+    print(result)
+    return result
+
+def invokeHandler():
+    startTime = int(round(time() * 1000))
+    count = 0
+    for i in range(1,20000000):
+        count +=1
+        ret = count
+
+        #ret = handler({'n': 100})
+    retTime = int(round(time() * 1000))
+
+    output = {'results': ret,
+        'startTime': startTime,
+        'retTime' : retTime,
+        'invokeTime': startTime
+        }
+    logf = open("log.txt", "w")
+    logf.write(str(output))
+
+    print(output)
+
+if __name__ == "__main__":
+    invokeHandler()
